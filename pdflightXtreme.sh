94dc6ee2d0bb50e865a857c36d9a554d1e2f2d29
amaro@upv.es <amaro@riseup.net>
2024-04-28 17:24:06 +0200

    modified pdflightXtreme

--- pdflightXtreme.sh
+++ pdflightXtreme.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
+# Make a copy
 cp $1 $1_bak
 
+# Crop pdf
+pdfcrop $1 $1
+
+# Convert to png
 gs -dQUIET -dBATCH -dNOPAUSE -dNOPROMPT -sDEVICE=png16m \
    -dTextAlphaBits=4 -dGraphicsAlphaBits=4 "-r300x300"  \
    -sOutputFile=`basename $1 .pdf`.png $1
 
+# Convert to pdf
 convert `basename $1 .pdf`.png $1
+
+# Remove png
 rm `basename $1 .pdf`.png
